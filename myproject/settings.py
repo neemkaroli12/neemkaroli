f16ae2215947f5a50366f4beaa171d6b44887321
sakshikumari431 <baliramsingh67@gmail.com>
2025-07-08 13:00:26 +0530

    Add Render domain to ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$+ip^r0$+n*5ui%2!cg$67_1!4a%y(0$8fyp8kikj^3zjlw^2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neemkaroli.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
