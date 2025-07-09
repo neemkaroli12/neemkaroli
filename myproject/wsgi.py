a05a370cd0009aaf73c6f77e62e12873368f3956
sakshikumari431 <baliramsingh67@gmail.com>
2025-07-09 14:24:13 +0530

    initail commit

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -6,13 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application()  # ✅ First define it
+application = WhiteNoise(application)  # ✅ Then wrap it
+
