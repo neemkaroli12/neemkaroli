aabcd5b0707f7a9cca508715b312ecb29e2cde11
sakshikumari431 <baliramsingh67@gmail.com>
2025-07-09 10:50:30 +0530

    initail commit

--- my_app/views.py
+++ my_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_two(request):
-    return render(request,'index.html')
+    return render(request,'my_app/index.html')
 
 def about_seo(request):
-    return render(request,'seo.html')
+    return render(request,'my_app/seo.html')
