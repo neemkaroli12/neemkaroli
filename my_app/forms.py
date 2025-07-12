eb194d363473f173ae8bc3883e3201368321ba74
sakshikumari431 <baliramsingh67@gmail.com>
2025-07-12 10:42:11 +0530

    formadd commit

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Full Name ', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    phone = forms.CharField(label='Mobile Number', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Mobile Number'}))
-    designation = forms.CharField(label='Designation', required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Designation'}))
-    message = forms.CharField(label='Your Message', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Your Message'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Full Name'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email'
+    }))
+    phone = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Mobile Number'
+    }))
+    designation = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Designation'
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Your Message',
+        'style': 'height: 100px;'
+    }))
